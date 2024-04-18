62fd23bf5ebf3bd45819258b9bef111354b2d8d2
WorkingRaven2817 <164280595+WorkingRaven2817@users.noreply.github.com>
2024-04-17 19:53:47 -0600

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,16 @@
 
-//AGREGA TUS ENLACES DE FIREBASE AQUÍ
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyC2__vbPQ_Dg8PSn6oZPbONNFsMWx1KrL4",
+      authDomain: "practice-map-1b61f.firebaseapp.com",
+      projectId: "practice-map-1b61f",
+      storageBucket: "practice-map-1b61f.appspot.com",
+      messagingSenderId: "197735998479",
+      appId: "1:197735998479:web:96ebef9152e0453ade2f7a"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
